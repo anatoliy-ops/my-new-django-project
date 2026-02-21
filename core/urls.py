d47d351074cc41f9a3f2355a85346c5f2b9cb0d1
Анатолий <sasasavinoa@gmail.com>
2026-02-21 19:44:26 +0500

    Initial commit

---
+++ core/urls.py
@@ -0,0 +1,173 @@
+from django.contrib import admin
+from django.urls import path,re_path,include
+from django.views.generic import TemplateView
+
+from myapp  import views
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
+
+
+# urlpatterns = [
+#     path('',views.index),
+#     path('about',views.about),
+#     path('contact',views.contact)
+# ]
+
+
+
+# urlpatterns = [
+#     path('',views.index),
+#     path(r'^about',views.about),
+#     path(r'^contact',views.contact)
+# ]
+
+
+
+# urlpatterns = [
+#     re_path(r'^about/contact/',views.contact),
+#     re_path(r'^about/',views.about),
+#     path('',views.index),
+# ]
+
+
+# urlpatterns = [
+#     path('',views.index),
+#     path('about',views.about,kwargs={'name':'Tom','age':38}),
+# ]
+
+
+# urlpatterns = [
+#     path('index', views.index),
+# ]
+
+
+# urlpatterns = [
+#     path('',views.index,)
+# ]
+
+
+# urlpatterns = [
+#     path("", views.index),
+#     path("user", views.user),
+#     path("user/<name>", views.user),
+#     path("user/<name>/<int:age>", views.user),
+# ]
+
+
+# urlpatterns = [
+#     path("", views.index),
+#     re_path(r"^user/(?P<name>\D+)/(?P<age>\d+)", views.user),
+#     re_path(r"^user/(?P<name>\D+)", views.user),
+#     re_path(r"^user", views.user),
+# ]
+
+
+# product_patterns = [
+#     path("", views.products),
+#     path("new", views.new),
+#     path("top", views.top),
+# ]
+# urlpatterns = [
+#     path("", views.index),
+#     path("products/", include(product_patterns)),
+# ]
+
+
+# product_patterns = [
+#     path("", views.products),
+#     path("comments", views.comments),
+#     path("questions", views.questions),
+# ]
+# urlpatterns = [
+#     path("", views.index),
+#     path("products/<int:id>/", include(product_patterns)),
+# ]
+
+
+
+# urlpatterns = [
+#     path("", views.index),
+#     path("user/", views.user)
+# ]
+
+
+
+# urlpatterns = [
+#     path("", views.index),
+#     path("about/", views.about),
+#     path("contact/", views.contact),
+#     path("details/", views.details),
+# ]
+
+
+
+# urlpatterns = [
+#     path("index/<int:id>", views.index),
+#     path("access/<int:age>", views.access),
+# ]
+
+
+
+# urlpatterns = [
+#     path('',views.index),
+# ]
+
+
+
+
+# urlpatterns = [
+#     path('set',views.set),
+# ]
+
+
+
+# urlpatterns = [
+#     path('set',views.set),
+#     path('get',views.get),
+# ]
+
+
+# urlpatterns = [
+#     path('', views.index),
+# ]
+
+
+
+# urlpatterns = [
+#     path("about/", TemplateView.as_view(template_name="about.html")),
+#     path("contact/", TemplateView.as_view(template_name="contact.html")),
+# ]
+
+
+# urlpatterns = [
+#     path("about/", TemplateView.as_view(template_name="about.html",
+#         extra_context={"header": "О сайте"})),
+#     path("contact/", TemplateView.as_view(template_name="contact.html")),
+# ]
+
+
+# urlpatterns = [
+#     path("", views.index),
+# ]
+
+
+
+# urlpatterns = [
+#     path("", views.index),
+#     path('contacts/', views.contacts),
+# ]
+
+
+
+# urlpatterns = [
+#     path("", views.index),
+# ]
+
+
+
+urlpatterns = [
+    path("", views.index),
+    path('postuser/', views.postuser),
+]
---
+++ myapp/views.py
@@ -0,0 +1,242 @@
+from datetime import datetime
+
+from django.core.serializers.json import DjangoJSONEncoder
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect, HttpResponseForbidden, \
+    HttpResponseBadRequest, HttpResponseNotFound, JsonResponse
+from django.template.response import TemplateResponse
+
+
+# def index(request):
+#     return HttpResponse("Hello")
+
+
+
+# def index(request):
+#     return HttpResponse('<h2>Главная</h2>')
+# def about(request):
+#     return HttpResponse('<h2>О сайте</h2>')
+# def contact(request):
+#     return HttpResponse('<h2>Контакты</h2>')
+
+
+
+# def index(request):
+#     return HttpResponse('<h2>Главная</h2>')
+# def about(request,name,age):
+#     return HttpResponse(f''
+#                         f'<h2>О пользователе</h2>'
+#                         f'<p>Имя: {name}</p>'
+#                         f'<p>Возраст: {age}</p>')
+
+
+
+# def index(request):
+#     host=request.META['HTTP_HOST']
+#     user_agent = request.META['HTTP_USER_AGENT']
+#     path = request.path
+#     return HttpResponse(f"<p>Host: {host}</p>"
+#                         f"<p>Path: {path}</p>"
+#                         f"<p>User-Agent: {user_agent}</p>")
+
+
+
+# def index(request):
+#     return HttpResponse('Произошла ошибка',status=400,reason='Incorrect data')
+
+
+# def index(request):
+#     return HttpResponse("<h1>Hello World!</h1>",content_type="text/plain",charset='utf-8')
+
+
+# def index(request):
+#     return HttpResponse("<h2>Главная</h2>")
+# def user(request, name="Undefined", age =0):
+#     return HttpResponse(f"<h2>Имя: {name}  Возраст: {age}</h2>")
+
+
+# def index(request):
+#     return HttpResponse("<h2>Главная</h2>")
+# def user(request, name="Undefined", age =0):
+#     return HttpResponse(f"<h2>Имя: {name}  Возраст: {age}</h2>")
+
+
+# def index(request):
+#     return HttpResponse("Главная страница")
+# def products(request):
+#     return HttpResponse("Список товаров")
+# def new(request):
+#     return HttpResponse("Новые товары")
+# def top(request):
+#     return HttpResponse("Наиболее популярные товары")
+
+
+
+
+# def index(request):
+#     return HttpResponse("Главная страница")
+# def products(request, id):
+#     return HttpResponse(f"Товар {id}")
+# def comments(request, id):
+#     return HttpResponse(f"Комментарии о товаре {id}")
+# def questions(request, id):
+#     return HttpResponse(f"Вопросы о товаре {id}")
+
+
+# def index(request):
+#     return HttpResponse("<h2>Главная</h2>")
+# def user(request):
+#     age = request.GET.get("age", 0)
+#     name = request.GET.get("name", "Undefined")
+#     return HttpResponse(f"<h2>Имя: {name}  Возраст: {age}</h2>")
+
+
+# def index(request):
+#     return HttpResponse("Index")
+# def about(request):
+#     return HttpResponse("About")
+# def contact(request):
+#     return HttpResponseRedirect("/about")
+# def details(request):
+#     return HttpResponsePermanentRedirect("/")
+
+
+# def index(request, id):
+#     people = ["Tom", "Bob", "Sam"]
+#     if id in range(0, len(people)):
+#         return HttpResponse(people[id])
+#     else:
+#         return HttpResponseNotFound("Not Found")
+# def access(request, age):
+#     if age not in range(1, 111):
+#         return HttpResponseBadRequest("Некорректные данные")
+#     if (age > 17):
+#         return HttpResponse("Доступ разрешен")
+#     else:
+#         return HttpResponseForbidden("Доступ заблокирован: недостаточно лет")
+
+
+
+
+# def index(request):
+#     return JsonResponse({"name": "Tom", "age": 38})
+
+
+
+
+# def index(request):
+#     bob = Person("Bob", 41)
+#     return JsonResponse(bob, safe=False, encoder=PersonEncoder)
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+# class PersonEncoder(DjangoJSONEncoder):
+#     def default(self, obj):
+#         if isinstance(obj, Person):
+#             return {"name": obj.name, "age": obj.age}
+#         return super().default(obj)
+
+
+
+# def set(request):
+#     username = request.GET.get('username','Undefined')
+#     response =HttpResponse(f'Hello {username}')
+#     response.set_cookie('username',username)
+#     return response
+
+
+
+# def set(request):
+#     username = request.GET.get('username','Undefined')
+#     response =HttpResponse(f'Hello {username}')
+#     response.set_cookie('username',username)
+#     return response
+# def get(request):
+#     username = request.COOKIES['username']
+#     return HttpResponse(f'Hello {username}')
+
+
+# def index(request):
+#     return render(request,'index.html')
+
+
+# def index(request):
+#     return TemplateResponse(request, 'index.html')
+
+
+
+# def index(request):
+#     data={'header':'Hello Django','message':'Welcome to Python'}
+#     return render(request,'index.html',context=data)
+
+
+
+# def index(request):
+#     header='User data'
+#     langs=['Python','C++','C#']
+#     user={'name':'Sammy','age':23}
+#     address={'Furkat',23,45}
+#     data={'header':header,'langs':langs,'user':user,'address':address}
+#     return render(request,'index.html',context=data)
+
+
+
+# def index(request):
+#     return render(request, "index.html",context={'body':'<h1>Hello World!</h1>'})
+
+
+
+# def index(request):
+#     return render(request, "index.html")
+
+
+# def index(request):
+#     data={'n':0}
+#     return render(request,'index.html',context=data)
+
+
+# def index(request):
+#     langs=['Python','JavaScript','Java','C#','C++']
+#     return render(request, "index.html",context={'langs':langs})
+
+
+# def index(request):
+#     data={'red':'красный','green':'зеленый','blue':'cиний'}
+#     return render(request,'index.html',{'data':data})
+
+
+
+# def index(request):
+#     return render(request, "index.html")
+
+
+
+# def index(request):
+#     return render(request, "index.html", context={"my_date": datetime.now()})
+
+
+
+# def index(request):
+#     return render(request, "index.html")
+
+
+
+# def index(request):
+#     return render(request, "index1.html")
+# def contacts(request):
+#     return render(request, "contacts.html")
+
+
+
+# def index(request):
+#     return render(request, "index.html",context={'site':'METANIT.COM'})
+
+
+
+def index(request):
+    return render(request, "index.html")
+def postuser(request):
+    name=request.POST.get("name",'Undefined')
+    age=request.POST.get("age",1)
+    return HttpResponse(f"<h2>Name: {name}  Age: {age}</h2>")
