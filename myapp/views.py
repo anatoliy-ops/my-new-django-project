e3ef22d07c0a1d048678d20b44505db5750febd7
Анатолий <sasasavinoa@gmail.com>
2026-02-22 19:22:39 +0500

    fix comments

--- myapp/views.py
+++ myapp/views.py
@@ -264,13 +264,13 @@ from .forms import UserForm
 
 
 
-# def index(request):
-#     if request.method == "POST":
-#         name = request.POST.get("name")
-#         age = request.POST.get("age")
-#         return HttpResponse(f"<h2>Привет, {name}, твой возраст: {age}</h2>")
-#     else:
-#         userform = UserForm()
-#         return render(request, "index.html", {"form": userform})
+def index(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+        return HttpResponse(f"<h2>Привет, {name}, твой возраст: {age}</h2>")
+    else:
+        userform = UserForm()
+        return render(request, "index.html", {"form": userform})
 
 
