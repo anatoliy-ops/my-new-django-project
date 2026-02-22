7d9d8b81a471765a4829ad84be51d9c77a91ccaa
Анатолий <sasasavinoa@gmail.com>
2026-02-22 11:43:11 +0500

    update allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ajvd^7^($!5m-y^m*-2vok-nsz$@m@z6jo@z!9lr55a@i@ta6h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anatoliy228.pythonanywhere.com/']
 
 
 # Application definition
