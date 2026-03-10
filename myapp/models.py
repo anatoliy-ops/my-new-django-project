da92e32fb8ac46b5b5c440a5e3b325c0caefd661
Анатолий <sasasavinoa@gmail.com>
2026-03-10 21:43:51 +0500

    error

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models,migrations
-# class Person(models.Model):
-#     name = models.CharField(max_length=20)
-#     age = models.IntegerField()
+class Person(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
 class Order(models.Model):
     datetime = models.DateTimeField()
 class Company(models.Model):
